e3aef13bfc020bee2f1fcdc8784560a6e3807f1b
Justin Mui <justin.mui25@ycdsbk12.ca>
2024-02-22 12:48:50 -0500

    done

--- AreaSin.java
+++ AreaSin.java
@@ -2,12 +2,29 @@ class AreaSin extends ConsoleProgram {
 
   /**
   * Description
-  * @author:
+  * calculate the area of a triangle, given two sides and the contained angle
+  * @author: Justin M
   */
   
   public void run() {
-    
+
     // start coding here
-    
+
+    // Variables
+    double dblSideA;
+    double dblSideB;
+    double dblAngleC;
+    double dblArea;
+
+    // Asigning 
+    dblSideA = readDouble("What is side A: ");
+    dblSideB = readDouble("What is side B: ");
+    dblAngleC = readDouble("What is Angle in degrees C: ");
+
+    // Calculations
+    dblArea = (dblSideA *  dblSideB * Math.sin(Math.toRadians(dblAngleC)))/2;
+
+    // Outputs 
+    System.out.println("Area is: " + dblArea);
   }
 }
