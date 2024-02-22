162be85c5a32dffde17bd06c3d0461118fd9322c
Justin Mui <justin.mui25@ycdsbk12.ca>
2024-02-22 12:57:24 -0500

    done

--- Circumference.java
+++ Circumference.java
@@ -2,12 +2,23 @@ class Circumference extends ConsoleProgram {
 
   /**
   * Description
-  * @author:
+  * Computes the circumference of a circle given a radius from user entry.
+  * @author: Justin M
   */
   
   public void run() {
     
-    // start coding here
-    
+    // Variables
+    double dblRadius;
+    double dblCircumference;
+
+    // Input
+    dblRadius = readDouble("What is the radius of your circle? ");
+
+    // Calculations
+    dblCircumference = 2 * Math.PI * dblRadius;
+
+    // Output5
+    System.out.println("The circumference is "+ dblCircumference);
   }
 }
--- Cos3rd.java
+++ Cos3rd.java
@@ -2,6 +2,7 @@ class Cos3rd extends ConsoleProgram {
 
   /**
   * Description
+  * Lets you enter two sides of a triangle and the contained angle, and that will use the cosine law to calculate the third side
   * @author:
   */
   
